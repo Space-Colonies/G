374ef64a286d92ccfa46459f11897b7a9a688f00
sundayib <sunda049@gmail.com>
2025-03-08 16:01:21 +0100

    Add files via upload

---
+++ yt.js
@@ -0,0 +1,59 @@
+
+var list=[{Title:'Best trailers part 1',id:'xS-9D5PuEx4'},{Title:'Best trailers part 2',id:'id1'},{Title:'Best trailers part 3',id:'id2'},{Title:'Best trailers part 4',id:'id3'},{Title:'Best trailers part 5',id:'id4'},{Title:'Best trailers part 6',id:'id5'},{Title:'Best trailers part 7',id:'id6'},{Title:'Best trailers part 8',id:'id7'}];
+
+var player,isReady=false;
+function onYouTubeIframeAPIReady() {
+  
+  player = new YT.Player('player', {
+    
+    //videoId: 'xS-9D5PuEx4',
+    playerVars: { 'rel':0,'showInfo':0,'autoplay': 0,'fs':1,'allowFullScreen':1, 'allowSeekAhead':0,'controls': 1},
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange,
+      'onError': onPlayerError
+    }
+  });
+}
+
+function onPlayerReady(){ isReady=true;}
+
+function onPlayerStateChange(e)
+{
+  if(e.data===0)
+  {
+    clearInterval(counter);
+    if(count>player.getDuration())
+    {
+      
+      window.location.href="https://rtstg.com/"+'_'+player.getDuration();
+    }else{ document.getElementById('popmsg').innerHTML='Sorry, you fast fowarded the video and cannot be awarded the reward. Click the video title button again to watch without forwarding to receive your reward.'; document.getElementById('link').innerHTML=""; pop.style.display= "block";}
+    count=1;
+  }
+ /*player.getCurrentTime()
+  player.getVideoUrl()
+  player.getVideoEmbedCode()*/
+  
+}
+
+
+function onPlayerError(){ }
+
+
+function closePopUp(){  pop.style.display= "none"; }
+function watch(e){if( isReady) {count=1;counter= setInterval(Timer, 950); document.getElementById('player').scrollIntoView();   player.loadVideoById(e.id);} else{ document.getElementById('popmsg').innerHTML="Sorry, the player isn't ready yet. Try again in a few seconds"; document.getElementById('link').innerHTML=""; pop.style.display= "block";}}
+  
+function setVids()
+{ let s='';
+  for(let i=0;i<list.length;i++)s=s+ '<button class="bt3" onclick="watch(this);"id="'+list[i].id+'">'+ list[i].Title+'</button>';
+    document.getElementById('bts').innerHTML=s;
+  
+}
+
+setVids();
+
+var count=1;
+var counter;//= setInterval(Timer, 1000);
+function Timer() {count += 1;}
+
+
